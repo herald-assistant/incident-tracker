4084c494cb667258b2aedf0492d31fc94e0927d4
santand-poc <santand.poc@gmail.com>
2026-06-26 11:29:45 +0200

    flow explorer improvement

--- frontend/src/app/components/analysis-final-result/analysis-final-result.ts
+++ frontend/src/app/components/analysis-final-result/analysis-final-result.ts
@@ -1,13 +1,14 @@
-import { Component, input } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, input, signal } from '@angular/core';
 
 import { AnalysisResultResponse } from '../../core/models/analysis.models';
 import { hasMeaningfulValue } from '../../core/utils/analysis-display.utils';
 import { MarkdownContentComponent } from '../markdown-content/markdown-content';
 
+type AnalysisResultTab = 'functional' | 'technical';
+
 @Component({
   selector: 'app-analysis-final-result',
-  imports: [RouterLink, MarkdownContentComponent],
+  imports: [MarkdownContentComponent],
   templateUrl: './analysis-final-result.html',
   styleUrl: './analysis-final-result.scss'
 })
@@ -15,5 +16,10 @@ export class AnalysisFinalResultComponent {
   readonly result = input<AnalysisResultResponse | null>(null);
   readonly status = input('');
 
+  protected readonly activeAnalysisTab = signal<AnalysisResultTab>('functional');
   protected readonly hasMeaningfulValue = hasMeaningfulValue;
+
+  protected selectAnalysisTab(tab: AnalysisResultTab): void {
+    this.activeAnalysisTab.set(tab);
+  }
 }
